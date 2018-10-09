632b0eb205f04ecb12b32ee6c12c8a3ecc40de4d
rlreamy <34109594+rlreamy@users.noreply.github.com>
2018-10-09 10:50:54 -0400

    Added check to see if we have loaded the platform
    If not, load the platform defined in the mapping file.

--- load_expression.sh
+++ load_expression.sh
@@ -6,6 +6,17 @@ read studyName
 source vars
 dos2unix ./samples/studies/$studyName/expression.params
 mac2unix ./samples/studies/$studyName/expression.params
+dos2unix ./samples/studies/$studyName/expression/*.txt
+mac2unix ./samples/studies/$studyName/expression/*.txt
+
+PLATFORM=`sed -n '2p' samples/studies/$studyName/expression/*_Mapping.txt | cut -d ' ' -f2 | awk '{print $4}'`
+
+PLATFORM_LOADED=`psql -d transmart -c "select exists (select platform from deapp.de_gpl_info where platform = '$PLATFORM');" -tA`
+if [ $PLATFORM_LOADED = 't' ]; then
+        echo "Platform already loaded, skipping platform load"
+else
+        nohup make -C samples/postgres load_annotation_$PLATFORM > $PLATFORM.annotation.out &
+fi
 
 nohup make -C samples/postgres load_expression_$studyName > $studyName.expression.out &
 
